cb08a4576ad65805522353fbfe5705c684854b72
yboodhan <yboodhan@gmail.com>
2020-02-27 10:55:08 -0800

    Created and run tests for published recently function, modified code accordingly

--- polls/views.py
+++ polls/views.py
@@ -48,6 +48,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.db.models import F
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -57,8 +58,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
